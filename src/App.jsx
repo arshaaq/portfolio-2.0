77392e89701c7d1e6fe0943b70dbaded6731b320
Arshaaq Jiffry <scribcoding@gmail.com>
2023-08-19 23:00:25 -0400

    threejs practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,63 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { useEffect } from 'react';
-import svgTest from './assets/Lucky.svg'
+//import {Mesh} from "three";
+import svgTest from './assets/looool.svg'
 import './App.css'
 
+import {Canvas, useFrame} from "@react-three/fiber";
+import { PerspectiveCamera } from "@react-three/drei";
+import { Mesh } from "three";
+//fibre component
+function Threefibre(){
+
+  const cubeRef = useRef();
+
+
+  // //for animation
+  useFrame(()=>{
+    if(!cubeRef.current){
+      return;
+    }
+
+    cubeRef.current.rotation.x += 0.01;
+    cubeRef.current.rotation.y += 0.01;
+  });
+
+  return (
+    <mesh ref={cubeRef}>
+      <PerspectiveCamera />
+      <boxGeometry args={[3,3,3]}/>
+      <meshStandardMaterial color="lightblue" />
+    </mesh>
+  );
+};
+
+
 import gsap from 'gsap';
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
-  useEffect(()=>{
-        // Use GSAP to create animations
-        gsap.to(".path", {
-          duration: 1,
-          x: 100,
-          opacity: 0.5,
-          ease: 'power2.out',
-        });
-  },[]);
-
-
-
+  // useEffect(()=>{
+  //       // Use GSAP to create animations
+  //       gsap.to(".path", {
+  //         duration: 1,
+  //         x: 100,
+  //         opacity: 0.5,
+  //         ease: 'power2.out',
+  //       });
+  // },[]);
 
   return (
     <>
-
+    <Canvas>
+      <ambientLight intensity={1}/>
+      <pointLight position={[10, 10, 10]} />
+      <Threefibre />
+    </Canvas>
+      
       <header>
         <nav>
         <p>about</p>
