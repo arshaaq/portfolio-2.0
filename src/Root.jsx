1824e54cfb1073486099e489b446a3814ed35989
Arshaaq Jiffry <scribcoding@gmail.com>
2023-08-23 18:22:51 -0400

    test

--- src/Root.jsx
+++ src/Root.jsx
@@ -10,6 +10,11 @@ import Contact from './Contact.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
-    <Home/>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="about" element={<About/>} />
+      <Route path="projects" element={<Projects/>} />
+      <Route path="contact" element={<Contact/>} />
+    </Routes>
   </Router>
 )
