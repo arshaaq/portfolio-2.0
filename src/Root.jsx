3cd399f40c5b079be2745ed7d37ae6301a9002a1
Arshaaq Jiffry <scribcoding@gmail.com>
2023-08-23 16:09:03 -0400

    more githubs tests

--- src/Root.jsx
+++ src/Root.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/index.css'
 
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import {BrowserRouter, Routes, Route, HashRouter} from "react-router-dom"
 import Home from './Home.jsx'
 import About from './About.jsx'
 import Projects from './Projects.jsx'
 import Contact from './Contact.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
-      <Route path="/portfolio-2.0" element={<Home/>} />
-      <Route path="/portfolio-2.0/about" element={<About/>} />
-      <Route path="/portfolio-2.0/projects" element={<Projects/>} />
-      <Route path="/portfolio-2.0/contact" element={<Contact/>} />
+      <Route path="/" element={<Home/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/projects" element={<Projects/>} />
+      <Route path="/contact" element={<Contact/>} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 )
