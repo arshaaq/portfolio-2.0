44f7c985f1e9f4c7c4e3e3db646b857a37f28b0d
Arshaaq Jiffry <scribcoding@gmail.com>
2023-08-28 19:11:18 -0400

    gsap test

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import { Mesh } from "three";
 
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
-gsap.registerPlugin(ScrollTrigger);
+
 
 
 
@@ -33,27 +33,10 @@ import Contact from './components/Contact';
 
 function App() {
 
-  const container = useRef();
+
   const sections = document.querySelectorAll(".content-section");
   const navItems = document.querySelectorAll(".nav-item");
 
-  useEffect(()=>{
-
-    let containerContext = gsap.context(()=>{
-      gsap.to(".box", {
-        x: 200,
-        scrollTrigger: {
-          trigger: "#project",
-          start: "top top", // Animation starts when the top of .box reaches the vertical center of the viewport
-          end: "bottom center", // Animation ends when the bottom of .box reaches the vertical center of the viewport
-          scrub: true, // Causes the animation to smoothly follow the scroll position
-          markers: true// Shows markers for debugging purposes
-        },
-      });
-
-    },container.current);
-    return () => containerContext.revert(); 
-  },[]);
 
 
 
@@ -68,7 +51,7 @@ function App() {
   </header>
 
   <About/>
-  <Projects reference={container}/>
+  <Projects/>
   <Contact/>
 
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,18 +1,37 @@
-import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
 import ProjectItem from "./ProjectItem";
-import { useEffect, useState } from 'react';
-const tl = gsap.timeline();
+import { useEffect, useState, useRef } from 'react';
 
 
+function Projects(){
+  const container = useRef();
+  const box = useRef();
 
+  useEffect(()=>{
 
-function Projects({reference}){
+    let containerContext = gsap.context(()=>{
+      gsap.to(".box", {
+        x: 200,
+        scrollTrigger: {
+          trigger: "#projects",
+          start: "top top",
+          end: "bottom center", 
+          scrub: true, 
+          markers: true
+        },
+      });
+  
+    },container.current);
+    return () => containerContext.revert(); 
+  },[]);
+  
   const [dataArray, setDataArray] = useState([]);
 
 return(
   <>
 
-    <div id='projects' className='content-section' ref={reference}>
+    <div id='projects' className='content-section' ref={container}>
 
       <div className='box'>
 
